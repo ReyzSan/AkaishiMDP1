ac94dbf7b2069b485ef355453df33fea2eb31319
Rey <99199008+fvckreyz@users.noreply.github.com>
2022-02-19 00:21:15 -0800

    Add files via upload

--- plugins/my.js
+++ plugins/my.js
@@ -1,39 +1,29 @@
-const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = require('@adiwajshing/baileys')
+const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = require('@adiwajshing/baileys-md')
 let fs = require('fs')
 let handler = async (m) => {
     let who
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.sender
     else who = m.sender
     let user = global.db.data.users[who]
-let anu = `「 BALANCE 」`
-}
-let handler = async (m, { conn, usedPrefix: _p, command }) => {
-  try {
-    let _uptime = process.uptime() * 1000
-    let _muptime
-    if (process.send) {
-      process.send('uptime')
-      _muptime = await new Promise(resolve => {
-        process.once('message', resolve)
-        setTimeout(resolve, 1000)
-      }) * 1000
-    }
-    let muptime = clockString(_muptime)
-    let uptime = clockString(_uptime)
-    let totalreg = Object.keys(global.db.data.users).length
-    let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length
-    }
+let anu = `╭───❑ 「 BALANCE 」 ❑────
+│ ✇ Name: ${user.name}
+│ ✇ Limit: ${user.limit}
+│ ✇ Money: ${user.money}
+│ ✇ Exp: ${user.exp}
+│ ✇ Level: ${user.level}
+│ ✇ Role: ${user.role}
+╰❑`
      const template = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
      templateMessage: {
          hydratedTemplate: {
            hydratedContentText: anu,
            locationMessage: { 
            jpegThumbnail: fs.readFileSync('./media/bank.jpg') }, 
-           hydratedFooterText: '✇ Name: ${user.name}\n✇ Limit: ${user.limit}\n✇ Money: ${user.money}\n✇ Exp: ${user.exp}\n✇ Level: ${user.level}\n✇ Role: ${user.role}' ,
+           hydratedFooterText: wm,
            hydratedButtons: [{
              urlButton: {
-               displayText: 'Source Code',
-               url: `https://github.com/fvckreyz/AkaishimdP`
+               displayText: 'source code',
+               url: `${ApiiKey}`
              }
 
            },
